4c21f5db016dbc3b63979f8daca367ddbcd05002
Siva Sankar Kommerla <147400354+ksiva0@users.noreply.github.com>
2025-04-07 21:53:16 +0530

    Update config.py

--- email_assisstant/config.py
+++ email_assisstant/config.py
@@ -4,7 +4,7 @@ import streamlit as st
 # Accessing Streamlit secrets for sensitive data  
 GOOGLE_CLIENT_ID = st.secrets["google"]["client_id"]  
 GOOGLE_CLIENT_SECRET = st.secrets["google"]["client_secret"]
-GOOGLE_API_CREDENTIALS = st.secrets["credentials"]
+GOOGLE_CREDENTIALS = st.secrets["credentials"]
 
 # Initialize the database name and path  
 DB_NAME = 'email_assistant.db'  
