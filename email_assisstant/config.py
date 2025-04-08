3ddee3a70cf8a98a3160f671f5754ab3218e731f
Siva Sankar Kommerla <147400354+ksiva0@users.noreply.github.com>
2025-04-08 11:25:16 +0530

    Update config.py

--- email_assisstant/config.py
+++ email_assisstant/config.py
@@ -11,12 +11,12 @@ WEB_SEARCH_API_KEY = st.secrets["google"]["GOOGLE_API_KEY"]
 
 GOOGLE_OAUTH_CONFIG = {
     "installed": {
-        "client_id":"514002371701-cu3q3gji9opn0sadnhulu2c04d9d3gjr.apps.googleusercontent.com",
+        "client_id":GOOGLE_CLIENT_ID,
       "project_id":"able-math-455408-b1",
       "auth_uri":"https://accounts.google.com/o/oauth2/auth",
       "token_uri":"https://oauth2.googleapis.com/token",
       "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
-      "client_secret":"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",
+      "client_secret":GOOGLE_CLIENT_SECRET,
       "redirect_uris":["https://api.slack.com/apps/A08L8C9RZPU/oauth?"]
     }
 }
